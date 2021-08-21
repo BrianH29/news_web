0c4ae8fc15569d2276491b69a93a043024feb646
Brian Hwang <brian@Brianui-MacBookPro.local>
2021-08-21 17:22:26 +0900

    main news view

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
-    <h1>THIS IS MY FIRST NEWS</h1>
+    <ol>
+      <li v-for="news in news" :key="news.id">
+        <!-- <div>{{ news.title }}</div>
+        <span>{{ news.points }} points</span>
+        <span> by {{ news.user }} </span> -->
+        <div> {{ news.title}} </div>
+        <div>
+          <span>{{ news.points }} points</span>
+          <span> by {{ news.user }} </span>
+          <span>{{ news.time_ago }} | </span>
+          <span>comments : {{ news.comments_count }}</span>
+        </div>
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    news(){
+      return this.$store.state.news;
+    }
+  },
+  created() {
+    this.$store.dispatch('FETCH_NEWS'); 
+  }
 }
 </script>
 
-<style>
+<style scoped>
 h1{
   color:red;
+}
 
+li{
+  margin: 10px 0px 10px 0px; 
+}
+
+span{
+  color: #9c9898;
+  font-size: 12px; 
 }
 </style>
