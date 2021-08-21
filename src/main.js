f26a3b08c76953d9befa180f9dee20c91ac2e8a2
Brian Hwang <brian@Brianui-MacBookPro.local>
2021-08-21 15:23:26 +0900

    initial page setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootStrap from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootStrap); 
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/common/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-navbar variant="faded" type="light">
+        <b-navbar-brand href="#">
+          <img src="@/assets/logo.png" style="width:50px; height:50px" alt="Kitten">
+        </b-navbar-brand>
+      </b-navbar>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">News</b-nav-item>
+          <b-nav-item href="#">Newest</b-nav-item>
+          <b-nav-item href="#">Ask</b-nav-item>
+          <b-nav-item href="#">Show</b-nav-item>
+          <b-nav-item href="#">Jobs</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
